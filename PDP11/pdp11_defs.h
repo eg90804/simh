774832948f0230f3a2aecb90efd55e3f090aebe2
eg90804 <github@groenenberg.net>
2018-09-21 14:43:33 +0200

    remove duplicate

--- PDP11/pdp11_defs.h
+++ PDP11/pdp11_defs.h
@@ -490,7 +490,6 @@ typedef struct {
 #define DZ_MUXES        4                               /* max # of DZ muxes */
 #define VH_MUXES        4                               /* max # of VH muxes */
 #endif
-#define DZ_LINES        8                               /* lines per DZ mux */
 #define DLX_LINES       16                              /* max # of KL11/DL11's */
 #define DCX_LINES       16                              /* max # of DC11's */
 #define DUP_LINES       8                               /* max # of DUP11/DPV11's */
